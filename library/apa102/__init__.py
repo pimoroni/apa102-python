dab78708dc8c042f3144cfb4b00a6dbe73830b62
Phil Howard <phil@gadgetoid.com>
2020-07-03 11:25:17 +0000

    Add DocStrings, drop complexity, refine params

--- library/apa102/__init__.py
+++ library/apa102/__init__.py
@@ -6,18 +6,18 @@ __version__ = '0.0.1'
 
 
 class APA102():
-    def __init__(self, count=1, gpio_data=14, gpio_clock=15, gpio_cs=None, brightness=1.0, gpio=None, spi=None, use_spi=True):
+    def __init__(self, count=1, gpio_data=14, gpio_clock=15, gpio_cs=None, brightness=1.0, force_gpio=False):
         """Initialise an APA102 device.
+
+        Will use SPI if it's available on the specified data/clock pins.
 
         :param pixel_count: Number of individual RGB LEDs
         :param gpio_data: BCM pin for data
         :param gpio_clock: BCM pin for clock
         :param gpio_cs: BCM pin for chip-select
-        :param gpio: Optional GPIO back-end
-        """
+        :param force_gpio: Specify true to force use of GPIO bitbanging
 
-        self._gpio = gpio
-        self._spi = spi
+        """
 
         self._gpio_clock = gpio_clock
         self._gpio_data = gpio_data
@@ -41,11 +41,11 @@ class APA102():
         for _ in range(self._eof_length):
             self._buf.append(0b11111111)
 
-        if use_spi and gpio_data == 10 and gpio_clock == 11 and gpio_cs in (7, 8):
+        if not force_gpio and gpio_data == 10 and gpio_clock == 11 and gpio_cs in (7, 8):
             self._spi = spidev.SpiDev(0, [8, 7].index(gpio_cs))
             self._gpio_cs = None
 
-        elif use_spi and gpio_data == 20 and gpio_clock == 21 and gpio_cs in (18, 17, 16):
+        elif not force_gpio and gpio_data == 20 and gpio_clock == 21 and gpio_cs in (18, 17, 16):
             self._spi = spidev.SpiDev(0, [18, 17, 16].index(gpio_cs))
             self._gpio_cs = None
 
@@ -65,14 +65,33 @@ class APA102():
             time.sleep(0)
 
     def set_pixel(self, x, r, g, b):
+        """Set a single pixel
+
+        :param x: x index of pixel
+        :param r: amount of red (0 to 255)
+        :param g: amount of green (0 to 255)
+        :param b: amount of blue (0 to 255)
+
+        """
         offset = self._sof_length + (x * 4) + 1
         self._buf[offset:offset + 3] = [r, g, b]
 
     def set_brightness(self, x, brightness):
+        """Set global brightness of a single pixel
+
+        :param x: x index of pixel
+        :param brightness: LED brightness (0.0 to 1.0)
+
+        """
         offset = self._sof_length + (x * 4)
         self._buf[offset] = int(31 * brightness)
 
     def show(self):
+        """Display the buffer
+
+        Outputs the buffer to connected LEDs using either bitbanged GPIO or SPI.
+
+        """
         if self._gpio_cs is not None:
             self._gpio.output(self._gpio_cs, 0)
 
