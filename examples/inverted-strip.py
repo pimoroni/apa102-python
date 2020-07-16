1ea32b487e7bb7386cc61ebb0dd8187cadcb449a
Phil Howard <phil@gadgetoid.com>
2020-07-16 20:12:56 +0100

    Example for inverted LED strip

---
+++ examples/inverted-strip.py
@@ -0,0 +1,15 @@
+import time
+from apa102 import APA102
+from colorsys import hsv_to_rgb
+
+lights = APA102(120, 5, 6, None, brightness=0.5, invert=True)
+
+while True:
+    t = time.time() * 50
+    for x in range(120):
+        h = ((t + x) % 360) / 360.0
+        r, g, b = [int(c * 255) for c in hsv_to_rgb(h, 1.0, 1.0)]
+        lights.set_pixel(x, r, g, b)
+
+    lights.show()
+    time.sleep(1.0 / 60)
