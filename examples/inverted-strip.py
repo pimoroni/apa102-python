ee5198de3b0f0c2e090d2c1f3a29bedec51f42d6
Phil Howard <phil@gadgetoid.com>
2020-07-17 17:29:11 +0100

    Fix inverted strip to space hue out

--- examples/inverted-strip.py
+++ examples/inverted-strip.py
@@ -5,9 +5,9 @@ from colorsys import hsv_to_rgb
 lights = APA102(120, 5, 6, None, brightness=0.5, invert=True)
 
 while True:
-    t = time.time() * 50
+    t = time.time() * 0.1
     for x in range(120):
-        h = ((t + x) % 360) / 360.0
+        h = t + (x / 240.0)
         r, g, b = [int(c * 255) for c in hsv_to_rgb(h, 1.0, 1.0)]
         lights.set_pixel(x, r, g, b)
 
